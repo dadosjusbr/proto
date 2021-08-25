015e41bd6fc9a8e255b73804e678ce8b8ff5c044
Joeberth Souza <joeberth.souza@ccc.ufcg.edu.br>
2021-08-25 01:03:33 -0300

    adiciona unmarshall.go

--- csv/unmarshall.go
+++ csv/unmarshall.go
@@ -5,5 +5,7 @@ import (
 )
 
 func (c *Coleta_CSV) MarshalCSV() (string, error) {
-	return fmt.Sprintf("%s,%s,%d,%d,%v,%s,%s,%s", c.ChaveColeta, c.Orgao, c.Mes, c.Ano, c.TimestampColeta.Seconds, c.RepositorioColetor, c.VersaoColetor, c.DirColetor), nil
+	headers := fmt.Sprintln("chave_coleta,orgao,mes,ano,timestamp_coleta,repositorio_coletor,versao_coletor,dir_coletor")
+	row := fmt.Sprintf("%s,%s,%d,%d,%v,%s,%s,%s", c.ChaveColeta, c.Orgao, c.Mes, c.Ano, c.TimestampColeta.Seconds, c.RepositorioColetor, c.VersaoColetor, c.DirColetor)
+	return fmt.Sprintf("%s\n%s", headers, row), nil
 }
