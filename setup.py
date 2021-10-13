c1de7c352dad38ba1700e4ead8a4908c31918426
Manuel-Antunes <nerdbroz2@gmail.com>
2021-10-13 16:44:32 -0300

    fix: change proto version to 0.6 (proposed by: https://github.com/joaolgm)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='protoDadosjusbr',  
-     version='0.3',
+     version='0.6',
      author="Dadosjusbr",
      author_email="dadosjusbr@gmail.com",
      description="Contém esquema de dados e funções para os coletores do DadosjusBr",
