324c6e243ce4f887feabc494d25b60b2df84b93d
Daniel Fireman <danielfireman@gmail.com>
2021-11-05 14:55:09 +0000

    normalizando cabeçalhos de CSV

--- csv/unmarshall.go
+++ csv/unmarshall.go
@@ -4,6 +4,20 @@ import (
 	"fmt"
 )
 
+func (c *ContraCheque_CSV) MarshalCSV() (string, error) {
+	headers := fmt.Sprintln("id_contra_cheque,chave_coleta,nome,matricula,funcao,local_trabalho,tipo,ativo")
+	row := fmt.Sprintf("%s,%s,%s,%s,%s,%s,%s,%t",
+		c.IdContraCheque,
+		c.ChaveColeta,
+		c.Nome,
+		c.Matricula,
+		c.Funcao,
+		c.LocalTrabalho,
+		c.Tipo,
+		c.Ativo)
+	return fmt.Sprintf("%s%s", headers, row), nil
+}
+
 func (c *Coleta_CSV) MarshalCSV() (string, error) {
 	headers := fmt.Sprintln("chave_coleta,orgao,mes,ano,timestamp_coleta,repositorio_coletor,versao_coletor,dir_coletor")
 	row := fmt.Sprintf("%s,%s,%d,%d,%v,%s,%s,%s", c.ChaveColeta, c.Orgao, c.Mes, c.Ano, c.TimestampColeta.Seconds, c.RepositorioColetor, c.VersaoColetor, c.DirColetor)
@@ -26,5 +40,17 @@ func (c *Metadados_CSV) MarshalCSV() (string, error) {
 		c.DetalhamentoReceitaBase.String(),
 		c.DetalhamentoOutrasReceitas.String(),
 		c.DetalhamentoDescontos.String())
+	return fmt.Sprintf("%s%s", headers, row), nil
+}
+
+func (r *Remuneracao_CSV) MarshalCSV() (string, error) {
+	headers := fmt.Sprintln("id_contra_cheque,chave_coleta,natureza,categoria,item,valor")
+	row := fmt.Sprintf("%s,%s,%s,%s,%s,%f",
+		r.IdContraCheque,
+		r.ChaveColeta,
+		r.Natureza.String(),
+		r.Categoria,
+		r.Item,
+		r.Valor)
 	return fmt.Sprintf("%s%s", headers, row), nil
 }
